e286322820a1275a1fbcaa795f2c97d42fa7887e
luizhlucas <luizhlucaspereira@gmail.com>
2025-02-04 00:04:44 -0300

    portfolio-responsivo

---
+++ script.js
@@ -0,0 +1,15 @@
+let estadodoMenu = 0
+const lista = document.querySelector(".mobile-menu-list")
+
+function alternarMenu() {
+    estadodoMenu++
+
+    if (estadodoMenu%2 == 1){
+        lista.classList.add("open")
+        document.querySelector('.menu-icon').src = "/img/close_white_36dp.svg";
+    }
+    else {
+        lista.classList.remove("open")
+        document.querySelector('.menu-icon').src = "/img/menu_white_36dp.svg";
+    }
+}
